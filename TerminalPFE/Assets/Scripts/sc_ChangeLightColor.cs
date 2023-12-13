b70a85907dfb3c855fbea14979ea07d23c64dde4
Cooookizz <122864326+Cooookizz@users.noreply.github.com>
2023-12-13 15:30:04 +0100

    changement light color

--- TerminalPFE/Assets/Scripts/sc_ChangeLightColor.cs
+++ TerminalPFE/Assets/Scripts/sc_ChangeLightColor.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class sc_ChangeLightColor : MonoBehaviour
 {
+    public bool isOpen;
 
-    
+    public Material lightMat;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void ChangeColor(int indexMat)
+    {
+        lightMat = GetComponent<MeshRenderer>().materials[indexMat];
+
+        if (isOpen)//la porte est ouverte donc on ferme
+        {
+            lightMat.SetFloat("_ColorChanger", 0);
+            isOpen = false;
+        }
+
+        else //la porte est fermée donc on ouvre
+        {
+            lightMat.SetFloat("_ColorChanger", 1);
+            isOpen = true;
+        }
+    }
+
 }
