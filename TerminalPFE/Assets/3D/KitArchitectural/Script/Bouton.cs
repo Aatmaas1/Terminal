022476dedb6c29b52939b8227174769f6be3c978
UnTypeSansNom <hugo.chavant@gmail.com>
2023-12-19 07:51:36 +0100

    SAVE SYSTEM

--- TerminalPFE/Assets/3D/KitArchitectural/Script/Bouton.cs
+++ TerminalPFE/Assets/3D/KitArchitectural/Script/Bouton.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bouton : MonoBehaviour
+public class Bouton : MonoBehaviour, IDataManager
 {
     private UnityEventPortes UnityEventPortes;
     public GameObject PorteOuvertureParBouton;
+
+    public int index;
+    bool isOpen = false;
+
     private void Start()
     {
         UnityEventPortes = PorteOuvertureParBouton.GetComponent<UnityEventPortes>();
@@ -15,8 +19,61 @@ public class Bouton : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            isOpen = true;
             //print("La porte détecte le player en entrée" + transform.parent.name);
             UnityEventPortes.InteractDoorBouton();
         }
+    }
+
+    public void LoadData(GeneralData data)
+    {
+        switch (index)
+        {
+            case 0:
+                isOpen = data.porteTuto1Ouverte;
+                Debug.Log(name + isOpen);
+                if (isOpen)
+                {
+                    StartCoroutine(Ouvre());
+                }
+                break;
+            case 1:
+                isOpen = data.porteTuto2Ouverte;
+                if (isOpen)
+                {
+                    StartCoroutine(Ouvre());
+                }
+                break;
+            case 2:
+                isOpen = data.porteSortieOuverte;
+                if (isOpen)
+                {
+                    StartCoroutine(Ouvre());
+                }
+                break;
+        }
+    }
+
+    public void SaveData(ref GeneralData data)
+    {
+        switch (index)
+        {
+            case 0:
+                data.porteTuto1Ouverte = isOpen;
+                Debug.Log(name + isOpen);
+                break;
+            case 1:
+                data.porteTuto2Ouverte = isOpen;
+                break;
+            case 2:
+                data.porteSortieOuverte = isOpen;
+                break;
+        }
+    }
+
+    IEnumerator Ouvre()
+    {
+        yield return new WaitForSeconds(0.1f);
+        UnityEventPortes.InteractDoorBouton();
     }
 }
---
+++ TerminalPFE/Assets/Scripts/GestionMemoire/Data/GeneralData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+[System.Serializable]
+public class GeneralData
+{
+    public int lastSceneLoaded, indexterminal;
+    public Vector3 LastPos;
+    public bool porteTuto1Ouverte, porteTuto2Ouverte, porteSortieOuverte, SolCasse, hasSwitchedBody;
+
+
+    public GeneralData()
+    {
+        this.lastSceneLoaded = 0;
+        this.indexterminal = -1;
+        this.LastPos = Vector3.zero;
+        this.porteTuto1Ouverte = false;
+        this.porteTuto2Ouverte = false;
+        this.porteSortieOuverte = false;
+        this.SolCasse = false;
+        this.hasSwitchedBody = false;
+    }
+}
