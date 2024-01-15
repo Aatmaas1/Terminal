d8613fc3a9ecbb2cea811e987f0ed317e8d8f39a
Cooookizz <122864326+Cooookizz@users.noreply.github.com>
2024-01-15 11:02:45 +0100

    animation terminal

---
+++ TerminalPFE/Assets/Scripts/Objets/sc_AnimTerminal_LDOV.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_AnimTerminal_LDOV : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            animator.SetBool("IsOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("IsOpen", false);
+        }
+    }
+
+}
