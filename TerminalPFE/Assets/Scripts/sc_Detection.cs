547bc9f9d077a4549d3fb07dcc2fb439bf270163
Cooookizz <122864326+Cooookizz@users.noreply.github.com>
2024-01-15 18:11:33 +0100

    avancée detection

--- TerminalPFE/Assets/Scripts/sc_Detection.cs
+++ TerminalPFE/Assets/Scripts/sc_Detection.cs
@@ -7,6 +7,12 @@ public class sc_Detection : MonoBehaviour
 {
 
     public VisualEffect detectionFX;
+
+    public List<VisualEffect> interractableList;
+
+    public bool onDetection;
+
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +22,34 @@ public class sc_Detection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(onDetection)
+        {
+            foreach(VisualEffect fx in interractableList) 
+            {
+                fx.SetVector3("PlayerPos", player.position);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            player = other.transform;
+
             detectionFX.transform.position = other.transform.position;
             detectionFX.SendEvent("OnDetection");
+
+            onDetection = true;
+
+            foreach(VisualEffect fx in interractableList) 
+            {
+                fx.enabled = true;
+
+                Vector3 fxPos = fx.transform.position;
+                fx.SetVector3("Pos01", fxPos);
+                fx.SetVector3("Pos02", new Vector3(fxPos.x, fxPos.y+1f, fxPos.z));
+            }
         }
     }
 
