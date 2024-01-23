5dd45d07eefc045bf750dc3d353ea27ec268cebc
DannyIsWhatToDo <titouan.Vaillant@free.fr>
2024-01-23 11:38:31 +0100

    Ajustement text manager

--- TerminalPFE/Assets/Scripts/sc_TextAnimation_TV.cs
+++ TerminalPFE/Assets/Scripts/sc_TextAnimation_TV.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+
 public class sc_TextAnimation_TV : MonoBehaviour
 {
     public TextMeshProUGUI textcomponent;
     public string[] lignes;
+    public string[] lignes2;
+    public string[] lignes3;
+
+    public List<string[]> page = new List<string[]>();
     public float speedChar;
     public float speedLigne;
+    public float speedAttenteDansTexte;
 
     private int index;
-    public bool isoktoGO = false;
+    private int pageindex = 0;
+    
 
     private char retourligne = 'ù';
-    
+    private char AttenteDansTexte = '§';
+
+    public void Start()
+    {
+        page.Add(lignes); page.Add(lignes2); page.Add(lignes3);
+
+    }
     public void OnEnable()
     {
         textcomponent.text = string.Empty;
@@ -22,25 +36,31 @@ public class sc_TextAnimation_TV : MonoBehaviour
 
     public void StartAffichage()
     {
-        index = 0;
-        StartCoroutine(TypeLigne());
+        
+        StartCoroutine(TypeLigne(page[pageindex]));
     }
 
-    IEnumerator TypeLigne()
+    IEnumerator TypeLigne(string[] Lignes)
     {
-        foreach(char c in lignes[index].ToCharArray())
+        index = 0;
+        foreach (char c in Lignes[index].ToCharArray())
         {
             if(c == retourligne)
             {
                 textcomponent.text += "<br>";
                 yield return new WaitForSecondsRealtime(speedLigne);
             }
+            else if( c == AttenteDansTexte)
+            {
+                yield return new WaitForSecondsRealtime(speedAttenteDansTexte);
+            }
             else
             {
                 textcomponent.text += c;
+                yield return new WaitForSecondsRealtime(speedChar);
             }
             
-            yield return new WaitForSecondsRealtime(speedChar);
+            
             
         }
         NextLignes();
@@ -52,8 +72,24 @@ public class sc_TextAnimation_TV : MonoBehaviour
         {
             index++;
             print("prochaine ligne");
-            StartCoroutine(TypeLigne());
+            StartCoroutine(TypeLigne(page[pageindex]));
 
         }
+        else
+        {
+            ClearConsole();
+            
+        }
     }
+    void ClearConsole()
+    {
+        if(pageindex < page.Count -1)
+        textcomponent.text = string.Empty;
+        pageindex++;
+        print(pageindex);
+        StartCoroutine(TypeLigne(page[pageindex]));
+
+    }
+
+   
 }
