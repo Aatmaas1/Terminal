353bddc78e70603184afb9e3dc10157a7ec3b710
Cooookizz <122864326+Cooookizz@users.noreply.github.com>
2024-02-05 11:54:12 +0100

    VFX elden ring

---
+++ TerminalPFE/Assets/Scripts/VFXGestion/sc_BumperTrail_LDOV.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteInEditMode]
+public class sc_BumperTrail_LDOV : MonoBehaviour
+{
+
+    public Transform pos01;
+    public Transform pos02;
+    public Transform pos03;
+    public Transform pos04;
+
+    public VisualEffect bumperVFX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bumperVFX.SetVector3("Pos01", pos01.position);
+        bumperVFX.SetVector3("Pos02", pos02.position);
+        bumperVFX.SetVector3("Pos03", pos03.position);
+        bumperVFX.SetVector3("Pos04", pos04.position);
+    }
+}
