9269838d58e2c2d9308fe405210fe033c8513603
Cooookizz <122864326+Cooookizz@users.noreply.github.com>
2024-02-07 22:30:26 +0100

    vfx data

--- TerminalPFE/Assets/Scripts/Objets/sc_TriggerTuto_HC.cs
+++ TerminalPFE/Assets/Scripts/Objets/sc_TriggerTuto_HC.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class sc_TriggerTuto_HC : MonoBehaviour
 {
     public int index;
     bool isUsed = false;
+    VisualEffect _ZoneVFX;
+    bool alreadyActivated;
 
+    private void Start()
+    {
+        _ZoneVFX = GetComponent<VisualEffect>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player" && !isUsed)
@@ -14,7 +21,13 @@ public class sc_TriggerTuto_HC : MonoBehaviour
             isUsed = true;
             sc_TutoManager_HC.Instance.TriggerActivated(index);
             Debug.Log("Bye");
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
+            if(_ZoneVFX != null && !alreadyActivated)
+            {
+                _ZoneVFX.SendEvent("OnEnter");
+                _ZoneVFX.SetFloat("ColorChanger", 1);
+                alreadyActivated = true;
+            }
         }
     }
 }
