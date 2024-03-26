18e605e09f413d36bf58d3922379008aa51d6b4f
LESTOUXAmbroise <ambroise.lestoux@orange.fr>
2024-03-26 12:35:29 +0100

    repush de LD Ambroise sur 4NN4

---
+++ TerminalPFE/Assets/Scripts/UI Ambroise Rough/lowBatterie4NN4.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lowBatterie4NN4 : MonoBehaviour
+{
+    public GameObject BatterieLowEffect;
+
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            BatterieLowEffect.SetActive(true);
+        }
+    }
+}
