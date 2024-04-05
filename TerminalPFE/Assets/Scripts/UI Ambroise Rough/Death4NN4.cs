18b7aa8c81225b3ac79383555eb29d5216534043
Adrien MICHEL <adrimichel1512@gmail.com>
2024-04-05 09:49:18 +0200

    Modif de la fin de l'intro 4NN4

--- TerminalPFE/Assets/Scripts/UI Ambroise Rough/Death4NN4.cs
+++ TerminalPFE/Assets/Scripts/UI Ambroise Rough/Death4NN4.cs
@@ -7,6 +7,21 @@ public class Death4NN4 : MonoBehaviour
 {
     public Animator PlayerAnimator;
 
+    public GameObject fadeToBlack;
+
+    [Header("Contrôle des timings")]
+    public float timeToStop;
+    public float timeToDie;
+    public float timeToFade;
+    public float timeToLoad;
+
+    public AK_POSTEVENT_AM postEvent;
+
+    private void Start()
+    {
+        postEvent = GetComponent<AK_POSTEVENT_AM>();
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -18,13 +33,31 @@ public class Death4NN4 : MonoBehaviour
 
     IEnumerator WaitAnimDeath()
     {
+        PlayerAnimator.GetComponent<Animator>().SetTrigger("SlowWalk");
+        postEvent.PostEvent();
+
+        yield return new WaitForSeconds(timeToStop);
+
+        //PlayerAnimator.GetComponent<Animator>().SetTrigger("StopSlowWalk");
+
+        //En attendant
+        PlayerAnimator.GetComponent<Animator>().speed = 0.2f;
         sc_PlayerManager_HC.Instance.SetInputMode("Nothing");
+
+        yield return new WaitForSeconds(timeToDie);
+
+        PlayerAnimator.GetComponent<Animator>().speed = 1f;
         sc_ScreenShake.instance.ScreenBaseQuick();
         PlayerAnimator.GetComponent<Animator>().SetTrigger("Death");
 
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(timeToFade);
+
+        fadeToBlack.SetActive(true);
+
+        yield return new WaitForSeconds(timeToLoad);
         //sc_PlayerManager_HC.Instance.SetInputMode("Player");
+
         SceneManager.LoadScene("Introduction");
 
     }
