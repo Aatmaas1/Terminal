affc8a03088d51ee50b9093e150e37825963aaa7
LESTOUXAmbroise <ambroise.lestoux@orange.fr>
2024-04-05 11:02:05 +0200

    PushFinal avec les 2 planner 4NN4 et les line renderer bien set entre eux

--- TerminalPFE/Assets/Scripts/UI Ambroise Rough/Hurt4NN4.cs
+++ TerminalPFE/Assets/Scripts/UI Ambroise Rough/Hurt4NN4.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Hurt4NN4 : MonoBehaviour
 {
     public Animator PlayerAnimator;
+    public GameObject rope4NN4ToPlanner;
 
     public void OnTriggerEnter(Collider other)
     {
@@ -23,10 +24,11 @@ public class Hurt4NN4 : MonoBehaviour
         sc_PlayerManager_HC.Instance.SetInputMode("Nothing");
         sc_ScreenShake.instance.ScreenBaseQuick();
         PlayerAnimator.GetComponent<Animator>().SetTrigger("Hurting");
+        rope4NN4ToPlanner.SetActive(false);
 
 
         yield return new WaitForSeconds(7f);
         sc_PlayerManager_HC.Instance.SetInputMode("Player");
-        
+        rope4NN4ToPlanner.SetActive(true);
     }
 }
