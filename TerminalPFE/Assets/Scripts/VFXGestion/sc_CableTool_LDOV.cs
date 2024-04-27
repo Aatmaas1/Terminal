7958780245b08ec350560d690e8b840244fca171
Cooookizz <122864326+Cooookizz@users.noreply.github.com>
2024-04-27 23:31:50 +0200

    ajout de l'outil de création de cable

--- TerminalPFE/Assets/Scripts/VFXGestion/sc_CableTool_LDOV.cs
+++ TerminalPFE/Assets/Scripts/VFXGestion/sc_CableTool_LDOV.cs
@@ -9,15 +9,40 @@ public class sc_CableTool_LDOV : MonoBehaviour
 {
 
     public LineRenderer cable;
-    public AnimationCurve curve;
 
+    [Header("Les courbes qui permettent de modifier l'aspect du cable")]
+    [Header("t = 0 -> startPos; t = 1 -> endPos")]
+    [Tooltip("Modifie selon l'axe X")]
+    public AnimationCurve curveX;
+    [Tooltip("Modifie selon l'axe Y")]
+    public AnimationCurve curveY;
+    [Tooltip("Modifie selon l'axe Z")]
+    public AnimationCurve curveZ;
+
+    [Space]
+
+    [Tooltip("Pour modifier l'épaisseur du cable")]
     [Range(0f, 1f)]
     public float cableSize;
 
+    [Space]
+
+    [Tooltip("La position De départ")]
     public Transform startPos;
+    [Tooltip("La position De Fin")]
     public Transform endPos;
 
-    [Range(2, 20)]
+    [Space]
+
+    [Tooltip("Permet de changer le material du cable")]
+    public Material cableMat;
+    [Header("Agit comme le tiling & offset")]
+    public Vector2 textureScale = new Vector2(1,1);
+
+    [Space]
+
+    [Header("PLus de vertex -> meilleure correspondance avec la courbe")]
+    [Range(2, 50)]
     public int vertexNomber;
 
     // Start is called before the first frame update
@@ -42,9 +67,11 @@ public class sc_CableTool_LDOV : MonoBehaviour
                     float lerper = (i / (vertexNomber-1));
                     Vector3 pointPos = Vector3.Lerp(startPos.position, endPos.position, lerper);
 
-                    float modifY = pointPos.y + (curve.Evaluate(i/vertexNomber)-1);
+                    float modifX = pointPos.x + (curveX.Evaluate(i/vertexNomber)-1);
+                    float modifY = pointPos.y + (curveY.Evaluate(i/vertexNomber)-1);
+                    float modifZ = pointPos.z + (curveZ.Evaluate(i/vertexNomber)-1);
 
-                    Vector3 modifPos = new Vector3(pointPos.x, modifY, pointPos.z);
+                    Vector3 modifPos = new Vector3(modifX, modifY, modifZ);
 
                     int index = Mathf.FloorToInt(i);
                     cable.SetPosition(index, modifPos);
@@ -62,6 +89,13 @@ public class sc_CableTool_LDOV : MonoBehaviour
             sizeCurve.AddKey(0, cableSize);
             sizeCurve.AddKey(1, cableSize);
             cable.widthCurve = sizeCurve;
+
+            //le material
+            if(cableMat!= null)
+            {
+                cable.material = cableMat;
+                cable.textureScale = textureScale;
+            }
 
         }
     }
